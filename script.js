0f421d25bd2c35f9a24cbbbb1d2a9b13687cf715
ckyever <ckyeverg@gmail.com>
2025-09-15 16:02:13 +1000

    Add ability to handle numbers and operators

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+// Handle operations
+const ADD_OPERATOR = '+';
+const SUBTRACT_OPERATOR = '-';
+const MULTIPLY_OPERATOR = '×';
+const DIVIDE_OPERATOR = '÷';
+
+function operate(x, y, operator) {
+  let answer;
+  switch (operator) {
+    case ADD_OPERATOR:
+      answer = add(x, y);
+      break;
+    case SUBTRACT_OPERATOR:
+      answer = add(x, y);
+      break;
+    case MULTIPLY_OPERATOR:
+      answer = add(x, y);
+      break;
+    case DIVIDE_OPERATOR:
+      answer = add(x, y);
+      break;
+    default:
+      answer = null;
+      console.warn(`Unknown operator ${operator}`);
+  }
+
+  return answer;
+}
